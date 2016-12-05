b642eb04e1b9cdfe4d9569b7e398df27f82e2b0a
SahilTikale <sahil.tikale@gmail.com>
2016-12-05 12:47:49 -0500

    Adds some automation scripts

---
+++ mitm_https_lab/https_nginx_45373.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Following script starts an https nginx server listening on port 45373.
+#It should be placed in /root and setup to run on boot with root privileges.
+
+cd /root/git/EC521_project/mitm_https_lab/
+./run.sh &
+
+
---
+++ mitm_https_lab/setupmitm.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#Following script manipulates the IPtables to set rules that lets you
+#interact with the webserver (http and https) with or without mitmproxy
+
+#To interact with webserver without mitm, run setupmitm.sh -d
+#To route all packets to http or https server via mitm, run setupmitm.sh -i
+#To query the state of iptables, run setupmitm.sh -q 
+
+usage="$(basename "$0") [-h] [-on] [-off] -- Program activate/deactivate the setup for man-in-the-middle attack
+	using mitmproxy.
+
+where: 
+     -h 	Show this help text
+     -i		lets mitmproxy to intercept http/https communication
+     -d		Deactivates mitmproxy
+     -q		Queries the iptables to show the active rules. 
+
+Example:
+	./setupmitm.sh -i # will route all packets to http or https server via mitmproxy
+	./setupmitm.sh -d # will stop routing packets via mitmproxy
+	./setupmitm.sh -q # will let you query the iptable rules currently set. 
+"
+
+while getopts ':hidq' option; do
+  case "$option" in 
+    h) 	echo "$usage"
+	exit
+	;;
+    i)	echo "man-in-the-middle attack intiated"
+	echo "To intercept http, https connections start mitmproxy by running... "
+	echo "mitmproxy -T --host" 
+	/sbin/iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 45373 -j REDIRECT --to-port 8080
+	/sbin/iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 80 -j REDIRECT --to-port 8080
+
+	exit
+	;;
+    d)  echo "man in the middle attack stopped"
+	/sbin/iptables -t nat -F
+	exit
+	;;
+    q) 	echo "querying the iptables"
+	/sbin/iptables -t nat -L
+	exit
+	;;
+    \?) printf "illegal option: ~%s\n" "$OPTARG" >&2
+	echo "$usage" >&2
+	exit 1
+	;;
+  esac
+done
+
+
